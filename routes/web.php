32378d064860a1b775703d29b99f5c5cf3cef5e4
Quynh <tranquynhk1999@gmail.com>
2025-03-29 23:28:10 +0700

    Trang chủ +tìm phòng

--- routes/web.php
+++ routes/web.php
@@ -31,8 +31,8 @@ use App\Http\Controllers\CartController;
 Route::post('/cart/add', [CartController::class, 'addToCart'])->name('cart.add');
 
 use App\Http\Controllers\SearchController;
-Route::get('/search-room', [SearchController::class, 'showForm'])->name('searchroom.form'); // Hiển thị form
-Route::post('/search-room', [SearchController::class, 'searchRoom'])->name('searchroom.search'); // Xử lý tìm phòng
+Route::get('/search-room', [SearchController::class, 'showForm'])->name('searchroom.form'); // Hi?n th? form
+Route::post('/search-room', [SearchController::class, 'searchRoom'])->name('searchroom.search'); // X? l� t�m ph�ng
 Route::get('/booking-information', [SearchController::class, 'hienThiThongTin'])->name('thongtin');
 
 Route::post('/cart/add', [SearchController::class, 'addToCart'])->name('cart.add');
