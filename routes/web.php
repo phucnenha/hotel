0d2e2b09ab139cc46bb14216a0d16330254880b3
tienmeomeo <tiennn107@gmail.com>
2025-03-31 09:50:37 +0700

    update routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -30,15 +30,13 @@ Route::get('/fill_info', [BookingController::class, 'showForm'])->name('fill_inf
 use App\Http\Controllers\User\CartController;
 Route::post('/cart/add', [CartController::class, 'addToCart'])->name('cart.add');
 
+//--------------Tìm kiếm phòng--------------------------
+use App\Http\Controllers\SearchController;
+Route::get('/search-room', [SearchController::class, 'showForm'])->name('searchroom.form'); 
+Route::post('/search-room', [SearchController::class, 'searchRoom'])->name('searchroom.search'); 
+Route::get('/booking-information', [SearchController::class, 'hienThiThongTin'])->name('thongtin');
 
+// ----------------Giỏ hàng-----------------------
 Route::get('/cart', [CartController::class, 'index'])->name('cart');
 Route::get('/cart/remove/{room_id}', [CartController::class, 'remove'])->name('cart.remove');
 Route::get('/cart/checkout/{room_id}', [CartController::class, 'checkout'])->name('cart.checkout');
-//--------------Tìm kiếm phòng--------------------------
-use App\Http\Controllers\SearchController;
-Route::get('/search-room', [SearchController::class, 'showForm'])->name('searchroom.form'); // Hi?n th? form
-Route::post('/search-room', [SearchController::class, 'searchRoom'])->name('searchroom.search'); // X? l� t�m ph�ng
-Route::get('/booking-information', [SearchController::class, 'hienThiThongTin'])->name('thongtin');
-
-//Route::post('/cart/add', [SearchController::class, 'addToCart'])->name('cart.add');
-//Route::get('/cart', [SearchController::class, 'viewCart'])->name('cart.view');
