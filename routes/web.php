fb0094cd7d64c894539a02954086183c6ee48cf9
ThuThao004 <tranthithuthao204204@gmail.com>
2025-04-03 10:47:24 +0700

    Update route cho branch

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/thanhtoan', function () {
-    return view('pages.thanhtoan');
-});
+Route::get('/payment', [BookingController::class, 'showPaymentPage'])->name('paymentPage');
